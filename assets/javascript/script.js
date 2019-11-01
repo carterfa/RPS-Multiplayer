b9f67dcb1e2cdd4ce5027ce906589b8fd8253c75
Farrah Carter <carterfarraha@gmail.com>
2019-10-31 21:39:47 -0400

    Added basic gameplay code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,8 +8,22 @@ let p1Ready = false;
 let p2Ready = false;
 
 //compares player choices
-function check(){
-    console.log("rungame");
+function check() {
+    $("#message1").text(p1Choice + " VS " + p2Choice);
+    
+    if (p1Choice === p2Choice) {
+        //there is a tie
+        $("#message2").text("TIE!")
+        //player one wins
+    } else if (
+        (p1Choice === "ROCK" && p2Choice === "SCISSORS") ||
+        (p1Choice === "PAPER" && p2Choice === "ROCK") ||
+        (p1Choice === "SCISSORS" && p2Choice === "PAPER")) {
+        $("#message2").text("PLAYER 1 WINS!");
+    } else {
+        //player two wins
+        $("#message2").text("PLAYER 2 WINS!");
+    }
 
 }
 
@@ -24,7 +38,7 @@ $(document).ready(function () {
         p1Choice = $(this).text();
         //player 1 is now ready
         p1Ready = true;
-        console.log("P1"+p1Choice);
+        //console.log("P1" + p1Choice);
 
         //run check if other player is ready
         if (p2Ready === true) {
@@ -41,7 +55,7 @@ $(document).ready(function () {
         p2Choice = $(this).text();
         //player 2 is now ready
         p2Ready = true;
-        console.log("P2"+p2Choice);
+        //console.log("P2" + p2Choice);
 
         //run check if other player is ready
         if (p1Ready === true) {
